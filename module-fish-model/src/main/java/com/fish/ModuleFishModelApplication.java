39830ee357b52b51f38ba96daff7676e98693e8a
DavidZh0u <zhoudawei001@huazhu.com>
2018-12-10 11:39:40 +0800

    提交一个model

---
+++ module-fish-model/src/main/java/com/fish/ModuleFishModelApplication.java
@@ -0,0 +1,12 @@
+package com.fish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleFishModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleFishModelApplication.class, args);
+    }
+}
